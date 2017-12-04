b9a0cb74421621c43d1c5d8e625dfbf727500366
elena <casapciuce@gmail.com>
2017-12-05 01:50:25 +0200

    Last Scene

--- LudumDare40/Assets/Scripts/HeroHealth.cs
+++ LudumDare40/Assets/Scripts/HeroHealth.cs
@@ -9,10 +9,11 @@ public class HeroHealth : MonoBehaviour {
     public int waitingDie;
     int nrMouse = 0;
     int nrOfSprite = 0;
+    static int nrOfPuss = 1;
     public Sprite[] sprites;
     public GameObject hero;
-    public Text mouseScoreUI;
-    public GameObject deathScreen;
+    public Text mouseScoreUI,pussnr;
+    public GameObject deathScreen,winScreen;
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.CompareTag("Enemy"))
@@ -31,18 +32,31 @@ public class HeroHealth : MonoBehaviour {
 
             hero.GetComponent<HeroMovement>().moveSpeedy -= 0.4f;
 
-            if (nrMouse % 2 == 0 && nrMouse != 0)
+            if (nrMouse % 2 == 0 && nrMouse != 0 && nrMouse != 10)
             {
                 hero.GetComponent<SpriteRenderer>().sprite = sprites[nrOfSprite];
                 nrOfSprite++;
             }
             nrMouse++;
             mouseScoreUI.text = nrMouse.ToString();
+            if (nrMouse == 10)
+            {
+                pussnr.text = nrOfPuss.ToString();
+                winScreen.SetActive(true);
+                CameraMovement.moveSpeed = 0.0f;
+                hero.GetComponent<HeroMovement>().moveSpeed = 0.0f;
+            }
         }
     }
     IEnumerator WaitForDie()
     {
         yield return new WaitForSeconds(waitingDie);
+        nrOfPuss++;
+        SceneManager.LoadScene("scene1");
+    }
+    public void Restart()
+    {
+        nrOfPuss = 1;
         SceneManager.LoadScene("scene1");
     }
 }
